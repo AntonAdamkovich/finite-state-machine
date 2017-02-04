d05c6a57bf1accda1a7c2f8f17cdae01586024b6
AntonAdamkovich <anton.adamkovich@gmail.com>
2017-02-04 20:17:26 +0300

    finite-state-machine: add getState, changeState and reset methods

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,72 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        if(config){
+            this._config = config;
+            this._initial = this._config.initial;
+            this._previousState = null;
+        } else {
+            throw new TypeError("Invalid config");
+        }
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this._config.initial;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if(state in this._config.states){
+            this._previousState = this._config.initial;
+            this._config.initial = state;
+        } else {
+            throw new TypeError("State doesn't exist");
+        }
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        var states = this._config.states,
+        eventFound = false,
+        currentState,
+        transitions;
+
+        this._previousState = this._config.initial;
+
+        for(var state in states){
+            currentState = states[state];
+            transitions = currentState.transitions;
+
+            for(var transition in transitions){
+                if(event == transition){
+                    this._config.initial = transitions[transition];
+                    eventFound = true;
+                }
+            }
+        }
+
+        if(!eventFound){
+            throw new Error("Event doesn't exist");
+        }
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this._config.initial = this._initial
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,14 +76,42 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        var states = this._config.states,
+        currentState,
+        transitions,
+        result = [];
+
+        for(var state in states){
+            currentState = states[state];
+            transitions = currentState.transitions;
+
+            for(var transition in transitions){
+                if(event == transition){
+                    result.push(state);
+                }
+            }
+        }
+
+        if(!event){
+            result = Object.keys(states);
+        }
+        return result;
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        if(this._previousState){
+            this._config.initial = this._previousState;
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     /**
      * Goes redo to state.
